c3028a1d3ff85ebcbd973346fe90cef6a5248c97
Romain Herault <romain.herault@les-tilleuls.coop>
2024-10-30 11:32:51 +0100

    feat: hide instructions when pressing key

--- src/components/HUD.tsx
+++ src/components/HUD.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { UI, Stage, ticker } from '@alienkitty/space.js';
+import { UI } from '@alienkitty/space.js';
 import { css } from "../../styled-system/css";
 import { isMobile } from "react-device-detect";
 
@@ -7,6 +7,7 @@ const HUD = () => {
   const containerRef = useRef(null);
   const uiInstance = useRef(null);
   const animationFrameRef = useRef(null);
+  const isDetailsShown = useRef(true);
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -46,6 +47,15 @@ const HUD = () => {
 
     uiInstance.current.toggleDetails(true);
 
+    const handleKeyPress = () => {
+      if (isDetailsShown.current) {
+        uiInstance.current.toggleDetails(false);
+        isDetailsShown.current = false;
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyPress);
+
     // Append to our container
     containerRef.current.appendChild(uiInstance.current.element);
     // Setup animation loop
@@ -62,6 +72,8 @@ const HUD = () => {
       if (animationFrameRef.current) {
         cancelAnimationFrame(animationFrameRef.current);
       }
+      window.removeEventListener('keydown', handleKeyPress);
+
       if (uiInstance.current && uiInstance.current.element && containerRef.current) {
         containerRef.current.removeChild(uiInstance.current.element);
         uiInstance.current = null;
